ab1b0d57c84263e04812474d161e369b9d8e1eb8
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-06-10 16:20:51 +0100

    make sentence

---
+++ src/easy/makeSentence/solution.js
@@ -0,0 +1,17 @@
+// Given a sentence, capitalise the first letter and add a full stop to the end.
+//  However, if the sentence already ends with some form of puncutation, leave it as is.
+
+let string = "jack bauer is the best agent";
+
+const makeSentence = (string) => {
+  const capitalStr = string.charAt(0).toUpperCase() + string.slice(1);
+
+  let fullStop;
+  if (capitalStr.charAt(capitalStr.length - 1) != ".") {
+    fullStop = capitalStr + ".";
+  }
+  return fullStop;
+};
+
+const test = makeSentence(string);
+console.log(test);
